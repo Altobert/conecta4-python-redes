b09d502f292ec12a930b86ce768da784c1167ade
Altobert <alberto.sanmartin@usach.cl>
2023-11-06 20:13:54 -0300

    prueba de commit

---
+++ client.py
@@ -0,0 +1,110 @@
+import socket
+import json
+import sys 
+
+class Client:
+    def __init__(self):
+        self.current_board = []
+        self.current_player = ''
+        self.winner = ''
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        try:
+            self.sock.connect(('', 1213))
+            print("conectado")
+        except socket.error:
+            print("no pudo conectar")
+            exit(0)
+
+        self.sock.sendall(b'ready')
+        self.player_id = self.sock.recv(34).decode('utf-8')
+        print(self.player_id)
+
+        self.sock.recv(38)
+        
+        self.client_loop()
+
+    def client_loop(self):
+        while True:
+            self.sync_with_server()
+            self.print_board()
+
+            if(self.winner != '-'):
+                self.print_winner()
+                break
+
+            if(self.current_player == self.player_id):
+                move = self.get_player_move()
+                self.send_move(move)
+
+
+    def sync_with_server(self):
+        state = self.sock.recv(333)
+        print(state)
+        state = json.loads(state.decode('utf-8'))
+        self.current_board = state['board']
+        self.current_player = state['active']
+        self.winner = state['winner']
+
+
+    def print_winner(self):
+        print("\n")
+        win = """
+        ⭐⭐⭐⭐⭐⭐⭐
+        ⭐  UD. GANO  ⭐
+        ⭐⭐⭐⭐⭐⭐⭐
+                """
+        lose = """
+        ❌❌❌❌❌❌❌
+        ❌   UD. PERDIO  ❌
+        ❌❌❌❌❌❌❌
+        """
+
+        if(self.winner == self.player_id):
+            print(win)
+        else:
+            print(lose)
+
+    #funcion para imprimir tablero
+    def print_board(self):
+        output = ""
+        for row in self.current_board[::-1]:
+            for e in row:
+                if e == '-':
+                    output += '⚪️'
+                if e == 'A':
+                    output += '🔴'
+                if e == 'B':
+                    output += '🔵'
+            output += '\n'
+
+        output += ' 1 2 3 4 5 6 7 \n'
+
+        sys.stdout.write("\033[H")
+        sys.stdout.write("\033[2J")
+        sys.stdout.write("%s" % output)
+
+    def get_player_move(self):
+        valid_input = False
+
+        while(not valid_input):
+            message = input("Ingrese la columna de su preferencia: ")
+            col = message[0]
+            if(col.isdigit() and int(col) > 0 and int(col) <= len(self.current_board[0])):
+                valid_input = True
+            else:
+                print("\nInput invalido, intente otra vez")
+
+        col = str(int(col) - 1)
+        return col
+
+    def send_move(self, move):
+        message = self.player_id
+        message += move
+        message = bytes(message, 'utf-8')
+        print(message)
+        print(sys.getsizeof(message))
+        self.sock.sendall(message)
+
+if __name__ == "__main__":
+    Client()
